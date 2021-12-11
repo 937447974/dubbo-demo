ba34d0cd262a809fe116e75d34b77a22076f3263
阳君 <937447974@qq.com>
2021-12-11 22:32:08 +0800

    移除无用pom

--- consumer/src/main/java/com/dubbo/ConsumerApplication.java
+++ consumer/src/main/java/com/dubbo/ConsumerApplication.java
@@ -1,16 +1,12 @@
 package com.dubbo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.validation.annotation.Validated;
 
-@Slf4j
-@Validated
 @Configuration
 @SpringBootApplication
 @EnableAsync(proxyTargetClass = true)
@@ -21,7 +17,5 @@ public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
-
 
 }
